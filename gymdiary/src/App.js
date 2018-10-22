32903645bb237e8bd032e42d72f55ccd1c3fa069
Dillon Reinwald <dreinwaldmail@gmail.com>
2018-10-22 10:22:15 -0700

    login and sign up components

--- gymdiary/src/App.js
+++ gymdiary/src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Signup from "./components/Signup"
 
-const App = () => <Navbar />;
+const App = () => (
+    <div>
+        <Header />
+        <Navbar />
+        <Login />
+        <Signup />
+    </div>
+);
 
 export default App;
