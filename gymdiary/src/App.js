8a3cb5893843de8a1f75a7fbd0a5f02f9ee53ec8
Aaron D Pumphrey <apumpfree@gmail.com>
2018-10-25 11:24:11 -0700

    BAR CHART IS WORKING

--- gymdiary/src/App.js
+++ gymdiary/src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import Chart from "./components/chart";
+import Chart from "./components/Chart";
 
 const App = () => (
     <div>
--- gymdiary/src/components/chart.js
+++ gymdiary/src/components/chart.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
+import {Bar, Line, Pie} from 'react-chartjs-2';
+
+class Chart extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            chartData:{
+                labels: ['Back', 'Chest', 'Legs', 'Core'],
+                datasets:[
+                    {
+                    label: 'Weekly',
+                    data:[
+                        7,
+                        3,
+                        5,
+                        3
+                    ]
+                        
+                    
+                }
+
+                ]
+
+            }
+        }
+    }
 
 
-const Chart = () => (
-
-   <canvas id='myChart'></canvas>
-
-);
-
-let myChart
+    render() {
+        return (
+            <div className="barChart">
+            <Bar
+                data={this.state.chartData}
+                options={{
+                    maintainAspectRatio: false
+                }}
+            />
+            </div>
+        );
+    }
+}
 
 export default Chart;
